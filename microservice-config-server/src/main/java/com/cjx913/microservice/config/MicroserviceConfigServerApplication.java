ed9c08652d6ba9a8d389f17b19ab009b6dfca3a5
cjx913 <cjx913@126.com>
2018-12-28 20:22:39 +0800

    文件配置中心

---
+++ microservice-config-server/src/main/java/com/cjx913/microservice/config/MicroserviceConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.cjx913.microservice.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceConfigServerApplication.class, args);
+    }
+
+}
+
