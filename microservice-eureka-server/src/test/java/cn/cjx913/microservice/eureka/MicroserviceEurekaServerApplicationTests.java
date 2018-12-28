84629f3b172928d83ec35b2f7e520526ff152d61
cjx913 <cjx913@126.com>
2018-12-28 20:22:13 +0800

    服务注册中心

---
+++ microservice-eureka-server/src/test/java/cn/cjx913/microservice/eureka/MicroserviceEurekaServerApplicationTests.java
@@ -0,0 +1,17 @@
+package cn.cjx913.microservice.eureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroserviceEurekaServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
