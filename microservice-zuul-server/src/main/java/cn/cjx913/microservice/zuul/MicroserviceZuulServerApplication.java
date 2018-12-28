045e7b3b74a325bb0f07a628c6bc0b0e32daf600
cjx913 <cjx913@126.com>
2018-12-28 21:03:19 +0800

    zuul

--- microservice-zuul-server/src/main/java/cn/cjx913/microservice/zuul/MicroserviceZuulServerApplication.java
+++ microservice-zuul-server/src/main/java/cn/cjx913/microservice/zuul/MicroserviceZuulServerApplication.java
@@ -2,8 +2,11 @@ package cn.cjx913.microservice.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,6 +15,13 @@ public class MicroserviceZuulServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MicroserviceZuulServerApplication.class, args);
+    }
+
+    //对zuul使用config更新代理
+    @RefreshScope
+    @ConfigurationProperties("zuul")
+    public ZuulProperties zuulProperties(){
+        return new ZuulProperties();
     }
 
 }
